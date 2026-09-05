d1a3cd87c7d433e3afee9ea7579f51ea27cc9210
3181675mavarezjoannys-stack <3181675.mavarez.joannys@gmail.com>
2026-09-05 10:18:13 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,11 @@
+<?php
+$identificacion= $_POST ['identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$cargo=$_POST ['cargo'];
+$salario=$_POST ['salario'];
+echo "la identificacion del empleado es: ".identificacion;
+echo "los nombres y apellidos del empleado son: ".NomApellidos;
+echo "el cargo del empleado es: ".cargo;
+echo "el salario del empleado es: ".salario;
+?>
 
